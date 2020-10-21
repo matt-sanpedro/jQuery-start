bf3cdac9a08ac5555281ad4d1dbf1a48ea4bd0d3
matt-sanpedro <mattsanpedro76@gmail.com>
2020-10-20 23:23:27 -0700

    added jQuery examples including animations

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-$("h1").css("color", "red");
+$("h1").addClass("big-font");
+// $("button").text("Don't click me");
+// console.log($("h1").css("color"));
+
+// add attribute
+$("a").attr("href", "https://www.yahoo.com");
+
+// event listener
+$("h1").click(function () {
+  $("h1").css("color", "blue");
+});
+
+// // event listener will apply to all
+// $("button").click(function () {
+//   $("h1").css("color", "purple");
+// });
+
+// event listener for keypress
+$(document).keypress(function (event) {
+  // console.log(event.key);
+  $("h1").text(event.key);
+});
+
+// "on" method can use any javascript method
+$("h1").on("mouseover", function () {
+    $("h1").css("color", "red");
+});
+
+// website animations
+$("button").on("click", function () {
+    // $("h1").hide();
+    // $("h1").show();
+    // $("h1").toggle();
+    // $("h1").fadeOut();
+    // $("h1").fadeToggle();
+    // $("h1").slideToggle();
+
+    // can only add css that have a numeric value
+    $("h1").animate({opacity: 0.5}).animate({margin: 20});
+    // $("h1").animate({margin: 20});
+
+
+});
